3b6cb43ee758da81de7dfc54ba85502a63919d04
Michal Wozniak <2554268W@student.gla.ac.uk>
2022-10-15 12:35:57 +0100

    Google maps working

--- webapp_server/index.js
+++ webapp_server/index.js
@@ -1,39 +1,47 @@
 const express = require("express");   
 const path = require('path');
-//require csvtojson
-const csvtojson = require('csvtojson')
-//add file csv file names here and put the files in the "csv_files" folder
+const csvtojson = require('csvtojson');
+const fs = require('fs'); 
+require('dotenv').config({ path: path.join(__dirname+'/secret.env') });
+ 
 const csvfilepaths= ["security_logs.csv","people_data.csv","location_data.csv"]
-const fs = require('fs')
-
 const app = express();   
-
-const apiKey = "";
+const apiKey = process.env.APIKEY; 
 const PORT = 3000;  
 
 
  
-app.get("/" , (request,response) => { 
-    response.sendFile(path.join(__dirname+'/html/index.html'));
-}); 
+app.get("/campusmap" , (request,response) => { 
+    response.sendFile(path.join(__dirname+'/html/campusmap.html'));
+});   
+ 
+app.get("/getlocations" , (request, response) => {  
+    fs.readFile(path.join(__dirname+"/json_files/location_data.json"), "utf8", (error, jsonString) => {
+        if (error) {
+          console.log("File read failed:", error);
+          return;
+        }
+        response.json(jsonString);
+      }); 
+    }); 
+     
 
-//Reads data from csv files and converts them to json files and will be stored in json_files folder
-//checking commit account
-
-jsonfiles=[]
-for (let i = 0; i < csvfilepaths.length; i++){
-    jsonfiles[i]=csvfilepaths[i].replace(".csv",".json")
-    csvtojson()
-    .fromFile("csv_files/".concat(csvfilepaths[i]))
-    .then((json) =>{
-        fs.writeFileSync("json_files/".concat(jsonfiles[i]),JSON.stringify(json),"utf-8",(err)=>{
-            if(err) console.log(err)
+    
+ 
+app.listen(PORT,() => {   
+    jsonfiles=[]
+    for (let i = 0; i < csvfilepaths.length; i++){
+        jsonfiles[i]=csvfilepaths[i].replace(".csv",".json")
+        csvtojson()
+        .fromFile("csv_files/".concat(csvfilepaths[i]))
+        .then((json) =>{
+            fs.writeFileSync("json_files/".concat(jsonfiles[i]),JSON.stringify(json),"utf-8",(err)=>{
+                if(err) console.log(err)
+            }) 
         })
-        
-    })
-}
- 
+    }  
+     
 
- 
-app.listen(PORT,() => console.log(`Server listening on ${PORT}`));  
+    console.log(`Server listening on ${PORT}`)
+});  
 
