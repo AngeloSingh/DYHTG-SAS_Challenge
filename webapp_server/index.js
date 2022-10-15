213110da67631bb801faf29a5005f09c7ae17882
Angelo Singh <2470584s@student.gla.ac.uk>
2022-10-15 02:07:12 +0100

    added csv file folder where the files are read from and stored in json format inside json_files folder

--- webapp_server/index.js
+++ webapp_server/index.js
@@ -1,15 +1,35 @@
 const express = require("express");   
 const path = require('path');
+//require csvtojson
+const csvtojson = require('csvtojson')
+//add file csv file names here and put the files in the "csv_files" folder
+const csvfilepaths= ["security_logs.csv","people_data.csv","location_data.csv"]
+const fs = require('fs')
+
 const app = express();   
 
 const apiKey = "";
 const PORT = 3000;  
- 
+
+
  
 app.get("/" , (request,response) => { 
     response.sendFile(path.join(__dirname+'/html/index.html'));
 }); 
- 
+
+//Reads data from csv files and converts them to json files and will be stored in json_files folder
+jsonfiles=[]
+for (let i = 0; i < csvfilepaths.length; i++){
+    jsonfiles[i]=csvfilepaths[i].replace(".csv",".json")
+    csvtojson()
+    .fromFile("csv_files/".concat(csvfilepaths[i]))
+    .then((json) =>{
+        fs.writeFileSync("json_files/".concat(jsonfiles[i]),JSON.stringify(json),"utf-8",(err)=>{
+            if(err) console.log(err)
+        })
+        
+    })
+}
  
 
  
