b52c5e0678fb294b6b8958e0ddeb11c5a72566cb
Michal Wozniak <2554268W@student.gla.ac.uk>
2022-10-15 21:32:07 +0100

    Added user profiles and location picker

--- webapp_server/index.js
+++ webapp_server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require('path');
 const csvtojson = require('csvtojson');
 const fs = require('fs');  
-const { timeEnd } = require("console");
 require('dotenv').config({ path: path.join(__dirname+'/secret.env') });
 const csvfilepaths= ["security_logs.csv","people_data.csv","location_data.csv"]
 const app = express();   
@@ -24,19 +23,15 @@ app.get("/getlocations" , (request, response) => {
     }); 
       
      
-app.get("/gettime" , (request , response) => { 
-    console.log("getting time");  
-    console.log(request.query); 
+app.get("/gettime" , (request , response) => {   
     let minutesStart = request.query.minutesStart;
     let hoursStart = request.query.hoursStart; 
-    let start = parseInt(hoursStart+minutesStart);
+    let end = parseInt(hoursStart+minutesStart);
     let minutesEnd = request.query.minutesEnd;
     let hoursEnd = request.query.hoursEnd;  
-    let end = parseInt(hoursEnd + minutesEnd); 
+    let start = parseInt(hoursEnd + minutesEnd); 
     let building = request.query.building;
     let results = [];   
-     
-    console.log(building)
     
     fs.readFile(path.join(__dirname+"/json_files/security_logs.json"), "utf8", (error, jsonString) => {
         if (error) {
@@ -47,17 +42,22 @@ app.get("/gettime" , (request , response) => {
             for (i = 0 ; i < jsonString.length ; i++) { 
                 let time = jsonString[i].Time.split("-"); 
                 let timeStart = parseInt(time[0]); 
-                let timeEnd = parseInt(time[1]);   
+                let timeEnd = parseInt(time[1]);     
                 if (timeEnd > 2359) { 
                     timeEnd = 2359 + timeEnd;
                 } 
-                
-                if ( ((end <= timeStart && timeStart <= start ) || (end <= timeEnd && timeEnd <=start)) && jsonString[i].Location == building) { 
+ 
+                if ( ((start <= timeStart && timeStart <= end) || (start <= timeEnd && timeEnd <= end) || (start <= timeStart && timeStart <= end &&timeEnd >=start && timeEnd <= end) || (timeStart <= start && timeEnd >= end)) && jsonString[i].Location == building) { 
                     let person = { 
                         timeStart : timeStart,
                         timeEnd : timeEnd, 
                         name : jsonString[i].Name, 
-                        location : jsonString[i].Location, 
+                        location : jsonString[i].Location,  
+                        age : jsonString[i].Age, 
+                        gender : jsonString[i].Gender, 
+                        subject : jsonString[i].Subject, 
+                        height : jsonString[i].Height 
+
                     } 
                     results.push(person);
                 }
